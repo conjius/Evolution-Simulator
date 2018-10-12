12355548795249757acc3068b8bf09781667b7d1
Dais <daispaysagist@gmail.com>
2018-10-12 23:59:41 +0300

    Initial Commit of Unity project.

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public float CreaturesMoveSpeed;
+    public int NumberOfCreatures;
+    public GameObject CreaturePrefab;
+    private GameObject[] _creatures;
+
+    // Use this for initialization
+    private void Start()
+    {
+        _creatures = new GameObject[NumberOfCreatures];
+        for (var i = 0; i < NumberOfCreatures; ++i)
+        {
+            _creatures[i] = Instantiate(
+                CreaturePrefab,
+                new Vector3(Random.Range(-10f, 10f),
+                    Random.Range(1f, 5f),
+                    Random.Range(-10f, 10f)),
+                Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        for (var i = 0; i < NumberOfCreatures; ++i)
+        {
+            
+        }
+    }
+}
